34679102e8aab4bdd73d1964d873534758f6f1cc
eisacke <emily@isacke.com>
2015-09-14 09:43:05 +0100

    Added user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+UserSchema.methods.encrypt = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password){
+  return bcrypt.compareSync(password, this.password);
+};
+
+var User = mongoose.model("User", UserSchema);
+module.exports = User;
