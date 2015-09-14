5cac9b4c84093593cc3941f06988e547d6e16afd
eisacke <emily@isacke.com>
2015-09-14 11:35:51 +0100

    Got bower components linking

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+angular
+  .module('EventPlan', ['ngResource', 'ui.router', 'angular-jwt'])
+  .config(MainRouter);
+
+  angular
+  .module('EventPlan')
+  .run(function($http, $window){
+    var token = $window.localStorage.getItem('token');
+    $http.defaults.headers.common["Authorization"] = "Bearer " + token;
+  })
+
+  // Setup the routing with ui.router
+  function MainRouter($stateProvider, $urlRouterProvider){
+    $stateProvider
+      .state('signup', {
+        url: "/signup",
+        templateUrl: "js/views/signup.html"
+      })
+      .state('login', {
+        url: "/login",
+        templateUrl: "js/views/login.html"
+      })
+      .state('home', {
+        url: "/",
+        templateUrl: "js/views/homepage.html"
+      });
+
+    $urlRouterProvider.otherwise("/");
+  }
